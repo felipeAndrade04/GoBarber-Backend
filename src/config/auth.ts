aa93cc4e5fc1fafa54fe6a787eb75032604a4f16
Felipe Andrade <flpandrade2011@gmail.com>
2020-07-20 20:45:31 -0300

    Adicionando autenticação nas rotas de appointment

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'd59d6354b69b2f8e46d8b9ed98426539',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface RequestDTO {
@@ -30,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, 'd59d6354b69b2f8e46d8b9ed98426539', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
