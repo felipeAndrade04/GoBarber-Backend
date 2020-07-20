fa4ff8e966b9465594f41769037c20c9b867ada5
Felipe Andrade <flpandrade2011@gmail.com>
2020-07-20 19:51:36 -0300

    Adicionando validação de credenciais e geração de token JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: RequestDTO): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, 'd59d6354b69b2f8e46d8b9ed98426539', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
